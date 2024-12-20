d566813632889889e3ff92a81309d4129f00d051
zemanue <manureco.97@gmail.com>
2024-12-20 20:39:11 +0100

    Update begginer_exercises_moure.js
    Half of the exercise done

--- src/JavaScript/begginer_exercises_moure.js
+++ src/JavaScript/begginer_exercises_moure.js
@@ -1,17 +1,51 @@
 // 1. Escribe un comentario en una línea
 
+// Comentario en una línea
 
 // 2. Escribe un comentario en varias líneas
 
+/* 
+Comentario
+en varias
+líneas
+*/
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
-
+// Cadena de texto --> string
+let nombre = "Manu";
+// Número entero o decimal --> number
+let edad = 27;
+let altura = 1.73;
+// Booleano --> boolean
+let esEstudiante = true;
+// Indefinido --> undefined
+let indefinido;
+// Nulo --> null
+let nulo = null;
+// Símbolo --> symbol (valores únicos e inmutables)
+let simbolo = Symbol("simbolo");
+// Gran entero --> bigint (enteros mayores que 2^53 - 1)
+let granEntero = 9007199254740991n;
 
 // 4. Imprime por consola el valor de todas las variables
-
+console.log(nombre);
+console.log(edad); 
+console.log(altura);
+console.log(esEstudiante);
+console.log(indefinido);
+console.log(nulo);
+console.log(simbolo);
+console.log(granEntero);
 
 // 5. Imprime por consola el tipo de todas las variables
-
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof altura);
+console.log(typeof esEstudiante);
+console.log(typeof indefinido);
+console.log(typeof nulo);
+console.log(typeof simbolo);
+console.log(typeof granEntero);
 
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
 
