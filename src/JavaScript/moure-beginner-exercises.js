1b1f8bbb7bcda7f3b88b29681ef40625638ea4f8
zemanue <manureco.97@gmail.com>
2024-12-26 11:24:24 +0100

    Update moure-beginner-exercises.js
    Semicolons deleted

--- src/JavaScript/moure-beginner-exercises.js
+++ src/JavaScript/moure-beginner-exercises.js
@@ -12,58 +12,58 @@ líneas
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
 // Cadena de texto --> string
-let nombre = "Manu";
+let nombre = "Manu"
 // Número entero o decimal --> number
-let edad = 27;
-let altura = 1.73;
+let edad = 27
+let altura = 1.73
 // Booleano --> boolean
-let esEstudiante = true;
+let esEstudiante = true
 // Indefinido --> undefined
-let indefinido;
+let indefinido
 // Nulo --> null
-let nulo = null;
+let nulo = null
 // Símbolo --> symbol (valores únicos e inmutables)
-let simbolo = Symbol("simbolo");
+let simbolo = Symbol("simbolo")
 // Gran entero --> bigint (enteros mayores que 2^53 - 1)
-let granEntero = 9007199254740991n;
+let granEntero = 9007199254740991n
 
 // 4. Imprime por consola el valor de todas las variables
-console.log(nombre);
-console.log(edad); 
-console.log(altura);
-console.log(esEstudiante);
-console.log(indefinido);
-console.log(nulo);
-console.log(simbolo);
-console.log(granEntero);
+console.log(nombre)
+console.log(edad)
+console.log(altura)
+console.log(esEstudiante)
+console.log(indefinido)
+console.log(nulo)
+console.log(simbolo)
+console.log(granEntero)
 
 // 5. Imprime por consola el tipo de todas las variables
-console.log(typeof nombre);
-console.log(typeof edad);
-console.log(typeof altura);
-console.log(typeof esEstudiante);
-console.log(typeof indefinido);
-console.log(typeof nulo);
-console.log(typeof simbolo);
-console.log(typeof granEntero);
+console.log(typeof nombre)
+console.log(typeof edad)
+console.log(typeof altura)
+console.log(typeof esEstudiante)
+console.log(typeof indefinido)
+console.log(typeof nulo)
+console.log(typeof simbolo)
+console.log(typeof granEntero)
 
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
-nombre = "Paco";
-edad = 52;
-altura = 1.80;
-esEstudiante = false;
+nombre = "Paco"
+edad = 52
+altura = 1.80
+esEstudiante = false
 // indefinido se mantiene igual
 // nulo se mantiene igual
-simbolo = Symbol("otro simbolo");
-granEntero = 1007199254740992n;
-console.log(nombre);
-console.log(edad); 
-console.log(altura);
-console.log(esEstudiante);
-console.log(indefinido);
-console.log(nulo);
-console.log(simbolo);
-console.log(granEntero);
+simbolo = Symbol("otro simbolo")
+granEntero = 1007199254740992n
+console.log(nombre)
+console.log(edad)
+console.log(altura)
+console.log(esEstudiante)
+console.log(indefinido)
+console.log(nulo)
+console.log(simbolo)
+console.log(granEntero)
 
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
 
