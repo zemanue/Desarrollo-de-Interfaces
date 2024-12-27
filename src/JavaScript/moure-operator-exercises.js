a1dc89dd35a3871023894e3b3c60605eee175bc6
zemanue <manureco.97@gmail.com>
2024-12-27 22:21:45 +0100

    Update moure-operator-exercises.js

--- src/JavaScript/moure-operator-exercises.js
+++ src/JavaScript/moure-operator-exercises.js
@@ -1,20 +1,49 @@
+
 // 1. Crea una variable para cada operación aritmética
+let suma = 10 + 3
+let resta = 10 - 3
+let multiplicacion = 10 * 3
+let division = 10 / 3
+let modulo = 10 % 3
+let exponente = 10 ** 3
+let incremento = 0
+incremento++
+let decremento = 0
+decremento--
+console.log("Operaciones aritméticas:")
+console.log(suma)
+console.log(resta)
+console.log(multiplicacion)
+console.log(division)
+console.log(modulo)
+console.log(exponente)
+console.log(incremento)
+console.log(decremento)
 
 // 2. Crea una variable para cada tipo de operación de asignación,
 //    que haga uso de las variables utilizadas para las operaciones aritméticas
 
+
 // 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
+
 
 // 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
 
+
 // 5. Utiliza el operador lógico and
+
 
 // 6. Utiliza el operador lógico or
 
+
 // 7. Combina ambos operadores lógicos
+
 
 // 8. Añade alguna negación
 
+
 // 9. Utiliza el operador ternario
 
+
 // 10. Combina operadores aritméticos, de comparáción y lógicas
+
