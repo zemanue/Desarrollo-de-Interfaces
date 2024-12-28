ac4efc74f37d1470b717ec2a274908e63df5dbf9
zemanue <manureco.97@gmail.com>
2024-12-28 12:01:34 +0100

    Create moure-string-exercises.js

---
+++ src/JavaScript/CursoJSMoure/moure-string-exercises.js
@@ -0,0 +1,34 @@
+
+// 1. Concatena dos cadenas de texto
+let nombre = "Manu"
+let apellido = "Redondo"
+let nombreCompleto = nombre + " " + apellido
+console.log("Nombre completo: " + nombreCompleto)
+
+// 2. Muestra la longitud de una cadena de texto
+console.log("Longitud de nombre completo" + nombreCompleto.length)
+
+// 3. Muestra el primer y último carácter de un string
+console.log("Primer carácter: " + nombreCompleto.charAt(0))
+console.log("Último carácter: " + nombreCompleto.charAt(nombreCompleto.length - 1))
+
+// 4. Convierte a mayúsculas y minúsculas un string
+
+
+// 5. Crea una cadena de texto en varias líneas
+
+
+// 6. Interpola el valor de una variable en un string
+
+
+// 7. Reemplaza todos los espacios en blanco de un string por guiones
+
+
+// 8. Comprueba si una cadena de texto contiene una palabra concreta
+
+
+// 9. Comprueba si dos strings son iguales
+
+
+// 10. Comprueba si dos strings tienen la misma longitud
+
