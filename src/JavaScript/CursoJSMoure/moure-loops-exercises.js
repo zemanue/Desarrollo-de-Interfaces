826ca6ec08657b4150cf1fcdee36e26f84991390
zemanue <manureco.97@gmail.com>
2024-12-29 13:54:01 +0100

    Update moure-loops-exercises.js

--- src/JavaScript/CursoJSMoure/moure-loops-exercises.js
+++ src/JavaScript/CursoJSMoure/moure-loops-exercises.js
@@ -2,16 +2,29 @@
 // NOTA: Explora diferentes sintaxis de bucles para resolver los ejercicios
 
 // 1. Crea un bucle que imprima los números del 1 al 20
-
+for (let i = 0; i < 20; i++) {
+    console.log(i + 1)    
+}
 
 // 2. Crea un bucle que sume todos los números del 1 al 100 y muestre el resultado
-
+let suma = 0
+for (let i = 0; i < 100; i++) {
+    suma += i + 1
+}
+console.log(suma)
 
 // 3. Crea un bucle que imprima todos los números pares entre 1 y 50
-
+for (let i = 0; i < 50; i++) {
+    if ((i + 1) % 2 === 0) {
+        console.log(i + 1)    
+    }
+}
 
 // 4. Dado un array de nombres, usa un bucle para imprimir cada nombre en la consola
-
+let nombres = ["Manu", "Isa", "Raúl", "Dani", "Sara", "Alejandro"]
+for (const nombre of nombres) {
+    console.log(nombre)
+}
 
 // 5. Escribe un bucle que cuente el número de vocales en una cadena de texto
 
