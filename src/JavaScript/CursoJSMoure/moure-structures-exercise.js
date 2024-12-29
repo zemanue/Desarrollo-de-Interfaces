0a5531aec194c83483161ac4cf55053d61adbf90
zemanue <manureco.97@gmail.com>
2024-12-29 12:27:06 +0100

    Update moure-structures-exercise.js
    Set exercises finished

--- src/JavaScript/CursoJSMoure/moure-structures-exercise.js
+++ src/JavaScript/CursoJSMoure/moure-structures-exercise.js
@@ -13,10 +13,20 @@ console.log("Elemento eliminado: " + animalesArray.splice(2, 1))
 console.log("Resultado: " + animalesArray)
 
 // 4. Crea un set que almacene cinco libros
+let librosSet = new Set(["El Quijote", "1984", "El Señor de los Anillos", "El Resplandor", "Drácula"])
+console.log("Set de libros: ")
+console.log(librosSet)
 
 // 5. Añade dos más. Uno de ellos repetido
+librosSet.add("El Perfume")
+librosSet.add("El Quijote")
+console.log("Dos elementos añadidos. Resultado: ")
+console.log(librosSet)
 
 // 6. Elimina uno concreto a tu elección
+librosSet.delete("El Señor de los Anillos")
+console.log("Un elemento eliminado. Resultado: ")
+console.log(librosSet)
 
 // 7. Crea un mapa que asocie el número del mes a su nombre
 
