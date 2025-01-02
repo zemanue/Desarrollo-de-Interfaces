b3e29ccf1b3582ee0a91104847eebd17783baa71
zemanue <manureco.97@gmail.com>
2025-01-02 10:52:48 +0100

    Update moure-functions-exercises.js

--- src/JavaScript/CursoJSMoure/moure-functions-exercises.js
+++ src/JavaScript/CursoJSMoure/moure-functions-exercises.js
@@ -8,21 +8,54 @@ function sumarDosNumeros(a, b) {
 let a = 5, b = 3
 console.log(`Suma de ${a} + ${b} = ${sumarDosNumeros(a, b)}`)
 
+let sumarDosNumeros2 = (a, b) => a + b
+console.log(`Suma de ${a} + ${b} = ${sumarDosNumeros2(a, b)}`)
+
 // 2. Crea una función que reciba un array de números y devuelva el mayor de ellos
-function mayorNumeroDeArray(arrayDeNumeros) {
-    return Math.max(...arrayDeNumeros)
-}
+let mayorNumeroDeArray = (arrayDeNumeros) => Math.max(...arrayDeNumeros)
 
 arrayDeNumeros = [12, 53, 71, 1, 32, 83, 23, 88, 44, 8]
 console.log("Mayor número del array: " + mayorNumeroDeArray(arrayDeNumeros))
 
 // 3. Crea una función que reciba un string y devuelva el número de vocales que contiene
-
+let contarVocales = function (string) {
+    let vocales = 0
+    for (const letra of string) {
+        if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
+            vocales++
+        }
+    }
+    return vocales
+}
+let texto = "Ejemplo de texto"
+console.log(`Vocales dentro de "${texto}": ${contarVocales(texto)}`)
 
 // 4. Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas
+function arrayDeStringsAMayusculas(arrayDeStrings) {
+    let textoAMayusculas = arrayDeStrings.map(string => string.toUpperCase()) //map() recorre cada string del array y ejecuta la función de pasarlo a mayúsculas
+    return textoAMayusculas
+}
+
+let arrayDeStrings = ["Hola", "Mundo", "soy", "Manu"]
+console.log("Array de strings: " + arrayDeStrings)
+console.log("Array pasado a mayúsculas: " + arrayDeStringsAMayusculas(arrayDeStrings))
 
 
 // 5. Crea una función que reciba un número y devuelva true si es primo, y false en caso contrario
+let esPrimo = (number) => {
+    if (number < 2) {
+        return false
+    }
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            return false
+        }
+    }
+    return true
+}
+
+let numero = 97
+console.log(`El número ${numero} es primo: ${esPrimo(numero)}`)
 
 
 // 6. Crea una función que reciba dos arrays y devuelva un nuevo array que contenga los elementos comunes entre ambos
