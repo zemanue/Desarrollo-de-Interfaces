ee0201169df8a85bcb0cc4515922224a316e8a87
zemanue <manureco.97@gmail.com>
2025-01-23 16:43:19 +0100

    Create array2.js

---
+++ src/JavaScript/tareas-basicas/tarea-js-iv/array2.js
@@ -0,0 +1,6 @@
+/*
+2.Diseñar un array, con 5 imágenes. Se pregunta al usuario, cual desea imprimir, una vez que
+se teclea el nombre de la imagen, aparece en el documento. 
+*/
+
+let imagenes = ["moneras.jpg", "protistas.jpg", "hongos.jpg", "plantas.jpg", "animales.jpg"]
