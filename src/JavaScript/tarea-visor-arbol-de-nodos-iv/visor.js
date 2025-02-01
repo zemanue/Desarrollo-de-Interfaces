32f63a1f4024095a6e4fcf645a567045950331b7
zemanue <manureco.97@gmail.com>
2025-02-01 14:12:31 +0100

    Update visor.js
    When one of the small images below is clicked, it is seen in a larger size in the upper image

--- src/JavaScript/tarea-visor-arbol-de-nodos-iv/visor.js
+++ src/JavaScript/tarea-visor-arbol-de-nodos-iv/visor.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     let imagenAmpliada = document.getElementById("ampliada");
     
+    let imagenesVisor = document.querySelectorAll("#visorImagenes img")
     
-    
+    for (let i = 0; i < imagenesVisor.length; i++) {
+        imagenesVisor[i].addEventListener("click", function () {
+            imagenAmpliada.src = imagenesVisor[i].src;
+        })
+    }
 })
