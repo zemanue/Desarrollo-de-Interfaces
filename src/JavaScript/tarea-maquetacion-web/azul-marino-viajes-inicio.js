f3c3dea909f19dd56d0aadfba03c88bd53ca5c62
zemanue <manureco.97@gmail.com>
2025-02-13 10:51:54 +0100

    Refactor azul-marino-viajes (section 3 from JS)
    Used a for loop to create the section 3 content

--- src/JavaScript/tarea-maquetacion-web/azul-marino-viajes-inicio.js
+++ src/JavaScript/tarea-maquetacion-web/azul-marino-viajes-inicio.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Textos lorem ipsum
+    const loremIpsumCorto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    const loremIpsumLargo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu libero eu urna maximus congue. Donec pretium purus orci, ac placerat tellus placerat vitae. Proin condimentum scelerisque vestibulum. Integer auctor lectus ac mollis feugiat. Aenean ipsum enim, varius quis felis eget, maximus ullamcorper tellus. Nam condimentum diam at ligula dignissim porttitor. Integer a eros risus. Vestibulum ut ante molestie, suscipit sapien a, egestas risus. Fusce gravida molestie accumsan. Maecenas quis blandit felis."
+    
+    let parrafoSeccion1 = document.querySelector("#seccion1 p")
+    parrafoSeccion1.textContent = loremIpsumLargo
+
     // Imagen sección 1
     let imagenSeccion1 = document.getElementById("imagenSeccion1")
     imagenSeccion1.src = "articulos-808572.jpg"
@@ -9,12 +16,35 @@ document.addEventListener('DOMContentLoaded', function() {
     imagenSeccion2.src = "foto6 (2).jpg"
     imagenSeccion2.alt = "Imagen sección 2"
 
-    // Imágenes sección 3
-    let imagenesSeccion3 = document.querySelectorAll("#seccion3 img")
-    imagenesSeccion3[0].src = "Viajes_Donde_Viajes_3.jpg"
-    imagenesSeccion3[0].alt = "Imagen de bote en la playa"
-    imagenesSeccion3[1].src = "kayak-valencia.jpeg"
-    imagenesSeccion3[1].alt = "Imagen de kayak en Valencia"
-    imagenesSeccion3[2].src = "600x400_viajes-lujo-FOTO-1-White-Dessert-experiencia-Polo-Norte-en-jet-privaso.jpg"
-    imagenesSeccion3[2].alt = "Imagen de jet privado en el Polo Norte"
+    // Creacion de la sección 3
+    let imagenesSeccion3 = [
+        "Viajes_Donde_Viajes_3.jpg", 
+        "kayak-valencia.jpeg", 
+        "600x400_viajes-lujo-FOTO-1-White-Dessert-experiencia-Polo-Norte-en-jet-privaso.jpg"
+    ]
+    let altsSeccion3 = [
+        "Imagen de bote en la playa", 
+        "Imagen de kayak en Valencia", 
+        "Imagen de jet privado en el Polo Norte"
+    ]
+
+    for (let i = 0; i < imagenesSeccion3.length; i++) {
+        let seccion3 = document.getElementById("seccion3")
+        let div = document.createElement("div")
+        seccion3.appendChild(div)
+
+        let img = document.createElement("img")
+        img.src = imagenesSeccion3[i]
+        img.alt = altsSeccion3[i]
+        div.appendChild(img)
+
+        let h5 = document.createElement("h5")
+        h5.textContent = loremIpsumCorto
+        div.appendChild(h5)
+
+        let button = document.createElement("button")
+        button.textContent = "Comprar"
+        div.appendChild(button)
+    }
+
 })
